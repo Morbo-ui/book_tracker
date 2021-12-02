8be03e43caa97664d8550c32801c7f72c5087667
Morbo-ui <lbvf6000@gmail.com>
2021-12-02 23:54:09 +0300

    feat: search through the url add search field

--- src/main/java/ru/morboui/search/SearchController.java
+++ src/main/java/ru/morboui/search/SearchController.java
@@ -2,14 +2,20 @@ package ru.morboui.search;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class SearchController {
+
+    private final String COVER_IMAGE_ROOT = "http://covers.openlibrary.org/b/id/";
 
     private final WebClient webClient;
 
@@ -27,8 +33,23 @@ public class SearchController {
         Mono<SearchResult> resultMono = this.webClient.get()
                 .uri("?q={query}", query)
                 .retrieve().bodyToMono(SearchResult.class);
-                SearchResult result = resultMono.block();
-                model.addAttribute("searchResult", result);
+        SearchResult result = resultMono.block();
+        List<SearchResultBook> books = result.getDocs()
+                .stream()
+                .limit(10)
+                .map(bookResult -> {
+                    bookResult.setKey(bookResult.getKey().replace("/works", ""));
+                    String coverId = bookResult.getCover_i();
+                    if (StringUtils.hasText(coverId)) {
+                        coverId = COVER_IMAGE_ROOT + coverId + "-M.jpg";
+                    } else {
+                        coverId = "src/main/resources/static/images/no-image.jpg";
+                    }
+                    bookResult.setCover_i(coverId);
+                    return bookResult;
+                })
+                .collect(Collectors.toList());
+        model.addAttribute("searchResults", books);
 
         return "search";
     }
